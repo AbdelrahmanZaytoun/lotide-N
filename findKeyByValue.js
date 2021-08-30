a33722e2024c29ad562f356403fa55f2cc15d431
abdelrahman zaytoun <abdelrahmanzaytounn@gmail.com>
2021-08-29 23:14:03 -0400

    return an elements of array if they are matched with the value

---
+++ findKeyByValue.js
@@ -0,0 +1,13 @@
+const findKeyByValue = function(object, value) {
+    const keysArray = Object.keys(object);
+    for (let k of keysArray) {
+      
+
+      if (object[k] === value) {
+        
+        return item
+      }
+    }
+  };
+  
+  module.exports = findKeyByValue;
