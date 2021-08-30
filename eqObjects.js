c71fd59f82c9f4cbe00d5ceafa8562d04de1f25a
abdelrahman zaytoun <abdelrahmanzaytounn@gmail.com>
2021-08-29 22:56:59 -0400

    sadasda

---
+++ eqObjects.js
@@ -0,0 +1,21 @@
+const eqArrays = require('./eqArrays');
+
+const eqObjects = function(object1, object2) {
+  let arrayA = Object.keys(object1);
+  let arrayB = Object.keys(object2);
+  if (arrayA.length !== arrayB.length) {
+    return false;
+  }
+  for (const item  of arrayA) {
+    if (Array.isArray(object1[item]) && Array.isArray(object2[item])) {
+      return eqArrays(object1[item], object2[item]);
+    } else {
+      if (object1[item] !== object2[item]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+module.exports = eqObjects;
